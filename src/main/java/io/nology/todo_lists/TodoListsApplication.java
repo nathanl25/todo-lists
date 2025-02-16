cb9a5fd8b2931fbff9aa2cba5fb8d17b963214a3
Ippat <nathan.ly.study@gmail.com>
2025-02-17 08:31:25 +1100

    Created directory

---
+++ src/main/java/io/nology/todo_lists/TodoListsApplication.java
@@ -0,0 +1,13 @@
+package io.nology.todo_lists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nology/todo_lists/TodoListsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.todo_lists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
